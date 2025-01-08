20d26e5cfd3b3c2545f066f0c41d43e00f1b7fe8
Merlijnos <99071140@mydavinci.nl>
2025-01-08 11:34:14 +0100

    Added minimalistic loader and styling, with bouncing dot animation and fix unescaped apostrophe in ErrorBoundary

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import './firebase';
+import './index.css';
+import App from './App';
+import ErrorBoundary from './ErrorBoundary';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </BrowserRouter>
   </React.StrictMode>
 );
+
+// Optional: Measure performance
+reportWebVitals(console.log);
